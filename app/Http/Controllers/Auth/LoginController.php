0b5cb2daa3a1ea9cc606f8d308591c91c4fb8642
Leonor2004 <up202205796@up.pt>
2024-11-22 12:07:25 +0000

    logout redirect correct

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,7 +48,7 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('login')
+        return redirect()->route('home')
             ->withSuccess('You have logged out successfully!');
     }
 }
