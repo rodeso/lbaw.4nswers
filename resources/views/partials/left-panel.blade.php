930bfd1be2c0def6b13cd781c15a7a5da2fad8bb
pedroossantos <up202205900@up.pt>
2024-11-23 14:50:47 +0000

    fixed tags when no tag is followed

--- resources/views/partials/left-panel.blade.php
+++ resources/views/partials/left-panel.blade.php
@@ -1,9 +1,18 @@
+@php
+    $isForYouPage = Route::currentRouteName() === 'foryou';
+@endphp
+
+
 <aside class="fixed top-20 left-5 w-[15%] bg-[color:#4E0F35] p-6 rounded-lg shadow-lg">
     <nav>
-        <ul class="space-y-4 bg-[color:#C18A8A]">
+        <ul class="bg-[color:#C18A8A] rounded">
             <li>
-                <button class="text-lg font-bold w-full text-left px-4 py-2 rounded hover:bg-gray-300">
-                    <a href="{{ route('foryou') }}">4U</a>
+                <button class="text-lg font-bold w-full text-left px-4 py-2 rounded hover:bg-gray-300 {{ $isForYouPage ? 'text-[color:#FF006E] bg-[color:#4E0F35]' : 'bg-[color:#C18A8A] ' }}">
+                    @if ($isForYouPage)
+                        <a href="{{ route('home') }}">4U</a>
+                    @else
+                        <a href="{{ route('foryou') }}">4U</a>
+                    @endif
                 </button>
             </li>
             <li>
@@ -24,19 +33,19 @@
         </ul>
     </nav>
     <div class="mt-8">
-        @if (Auth::check())
+        @if (Auth::check() && !$user_tags->isEmpty())
             <h4 class="font-bold mb-4 text-[color:#C18A8A]">Subscribed Tags</h4>
+                @foreach ($user_tags as $tag)
+                        <li>
+                            <a class="text-[color:#C18A8A]">
+                                {{ $tag->name }}
+                            </a>
+                        </li>
+                @endforeach
+        @elseif (Auth::check())
+            <h4 class="font-bold mb-4 text-[color:#C18A8A]">No Tags Subscribed</h4>
         @else
             <a href="{{route('login')}}" class="font-bold mb-4 text-[color:#C18A8A]">Login to Subscribe to Tags</a>
         @endif
-        <ul class="space-y-2">
-            @foreach ($user_tags as $tag)
-                <li>
-                    <a class="text-[color:#C18A8A]">
-                        {{ $tag->name }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
     </div>
 </aside>
