d33dcb9e3c0b6a53b7c72d279ef8d5287aedb755
El-Castro <afonsocastroaf@gmail.com>
2024-12-06 11:47:18 +0000

    About Us' footer update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="bg-[color:#4E0F35] text-[color:#FF006E] text-center py-4 mt-8">
     <a href="{{ route('terms-and-conditions') }}">Copyright © 2024</a>
-    <a href="">4NSWERS</a>
+    <a href="{{ route('about.us') }}">4NSWERS</a>
 </footer>
