68bbe5722c22b6d9c4343949a056d277262e4904
pedroossantos <up202205900@up.pt>
2024-12-11 12:23:32 +0000

    admin page upgrade

--- resources/views/partials/user-card-admin.blade.php
+++ resources/views/partials/user-card-admin.blade.php
@@ -1,39 +1,67 @@
-<section class="w-[650px] h-[100px]">
+<section class="w-[1000px] h-[100px]">
     <!-- Post's Question -->
     <section class="w-auto h-25 bg-[color:#C18A8A] rounded-lg shadow-md p-3 space-y-2">
         <div class="relative flex space-x-[10px] items-center">
-            <div class="w-20 h-20 bg-gray-300 rounded-2xl ml-0">
+            <div class="w-20 h-20 bg-gray-300 rounded-2xl ml-0 overflow-hidden">
                 <img 
                     src="{{ $user->profile_picture ? asset('storage/' . $user->profile_picture) : asset('profile_pictures/5P31c2m0XosLV5HWAl8gTDXUm0vVmNO6ht8llkev.png') }}" 
                     alt="Profile Picture" 
-                    class="w-20 h-20 bg-gray-300 rounded-2xl !m-0"
+                    class="w-full h-full object-cover"
                 />
             </div>
-            <div class="relative flex items-center space-x-[100px] w-full p-4 ml-4 mr-4 bg-[color:#4B1414] rounded-md">
+            <div class="relative flex items-center w-full p-4 ml-4 mr-4 bg-[color:#4B1414] rounded-md">
                 <a 
                     href="{{ Auth::id() === $user->id ? route('profile') : route('user.profile', ['id' => $user->id]) }}" 
-                    class="ml-[50px] text-2xl text-white hover:text-gray-500"
+                    class="text-2xl ml-12 text-white hover:text-gray-500"
                 >
                     {{ $user->nickname }}
                 </a>
-                <form method="POST" action="{{ route('user.toggleMod', ['id' => $user->id]) }}">
-                    @csrf
-                    <button 
-                        type="submit" 
-                        class="px-4 py-2 text-2xl text-white bg-blue-500 rounded-lg hover:bg-blue-700"
-                    >
-                        {{ $user->is_mod ? 'Remove Mod' : 'Make Mod' }}
-                    </button>
-                </form>
-                <form method="POST" action="{{ route('user.toggleBlock', ['id' => $user->id]) }}">
-                    @csrf
-                    <button 
-                        type="submit" 
-                        class="px-4 py-2 text-2xl text-white bg-red-500 rounded-lg hover:bg-red-700"
-                    >
-                        {{ $user->is_blocked ? 'Unblock User' : 'Block User' }}
-                    </button>
-                </form>
+                <div class="absolute right-10 flex">
+                    
+                    @if ($user->is_admin)
+                        <span class="px-4 py-2 mr-10 text-xl w-40 text-center text-white bg-green-500 rounded-lg">Admin</span>
+                    @else
+                        <form method="POST" action="{{ route('user.toggleMod', ['id' => $user->id]) }}">
+                            @csrf
+                            <button 
+                                type="submit" 
+                                class="px-4 py-2 mr-10 text-xl w-40 text-white bg-blue-500 rounded-lg hover:bg-blue-700"
+                            >
+                                {{ $user->is_mod ? 'Remove Mod' : 'Make Mod' }}
+                            </button>
+                        </form>
+                    @endif
+                    
+
+                    @if ($user->is_admin)
+                        <span class="px-4 py-2 mr-10 text-xl w-40 text-center text-white bg-gray-700 rounded-lg">Not Available</span>
+                    @else
+                        <form method="POST" action="{{ route('user.toggleBlock', ['id' => $user->id]) }}">
+                            @csrf
+                            <button 
+                                type="submit" 
+                                class="px-4 py-2 mr-10 text-xl w-40 text-white bg-red-500 rounded-lg hover:bg-red-700"
+                            >
+                                {{ $user->is_blocked ? 'Unblock User' : 'Block User' }}
+                            </button>
+                        </form>
+                    @endif
+
+
+                    @if ($user->is_admin)
+                        <span class="px-4 py-2 text-xl w-40 text-center text-white bg-gray-700 rounded-lg">Not Available</span>
+                    @else
+                        <form method="POST" action="">
+                            @csrf
+                            <button 
+                                type="submit" 
+                                class="px-4 py-2 text-xl w-40 text-white bg-red-500 rounded-lg hover:bg-red-700"
+                            >
+                                Delete User
+                            </button>
+                        </form>
+                    @endif
+                </div>
             </div>
         </div>
     </section>
