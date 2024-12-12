2bd22da4839d7f1825e3e78d45d57575d671a54d
pedroossantos <up202205900@up.pt>
2024-12-12 18:39:28 +0000

    admin fixed? for small screens (skill issue but sure)

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -36,7 +36,7 @@
         <main class="flex-grow flex justify-center pt-20">
             <!-- Centered Posts Section -->
             <section class="w-3/5 space-y-8 grid place-items-center h-full text-white">
-                <h1 id="admins" class="text-2xl font-bold w-[1000px] bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Admins:</h1>
+                <h1 id="admins" class="text-2xl font-bold w-[800px] bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Admins:</h1>
                 @foreach ($users as $user)
                     @if ($user->is_admin)
                         @if ($user->id !== 0)
@@ -44,7 +44,7 @@
                         @endif
                     @endif
                 @endforeach
-                <h1 id="moderators" class="text-2xl font-bold w-[1000px] mt-8 bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Moderators:</h1>
+                <h1 id="moderators" class="text-2xl font-bold w-[800px] mt-8 bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Moderators:</h1>
                 @foreach ($users as $user)
                     @if (!$user->is_admin && $user->is_mod)
                         @if ($user->id !== 0)
@@ -52,7 +52,7 @@
                         @endif
                     @endif
                 @endforeach
-                <h1 id="blocked" class="text-2xl font-bold w-[1000px] mt-8 bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Blocked Users:</h1>
+                <h1 id="blocked" class="text-2xl font-bold w-[800px] mt-8 bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Blocked Users:</h1>
                 @foreach ($users as $user)
                     @if (!$user->is_admin && !$user->is_mod && $user->is_blocked)
                         @if ($user->id !== 0)
@@ -60,7 +60,7 @@
                         @endif
                     @endif
                 @endforeach
-                <h1 id="others" class="text-2xl font-bold w-[1000px] bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Other Users:</h1>
+                <h1 id="others" class="text-2xl font-bold w-[800px] bg-[color:#4B1414] p-3 rounded-lg shadow-lg">Other Users:</h1>
                 @foreach ($users as $user)
                     @if (!$user->is_admin && !$user->is_mod && !$user->is_blocked)
                         @if ($user->id !== 0)
