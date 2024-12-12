3586619c9ad3b63fd8473e2fbdcc7a0f8437bc3b
Leonor2004 <up202205796@up.pt>
2024-12-12 14:04:49 +0000

    header drawer menu solved

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -66,11 +66,9 @@
                     <button class="text-white mb-4" onclick="this.parentElement.parentElement.remove()">Close</button>
                     <h2 class="text-xl font-bold">${nickname} Menu</h2>
                     @if (Auth::check())
-                        <a href="{{ route('user.profile', ['id' => Auth::id()]) }}"><li class="py-2 hover:bg-gray-700 hover:text-[color:#FF006E] px-4 rounded">Profile</li></a>
-                        @if (Auth::user()->is_admin || Auth::user()->is_mod)
-                            <a href="{{ route('admin-dashboard') }}"><li class="py-2 hover:bg-gray-700 hover:text-[color:#FF006E] px-4 rounded">Admin Dashboard</li></a>
-                        @endif
-                        <a href=""><li class="py-2 hover:bg-gray-700 hover:text-[color:#FF006E] px-4 rounded">Settings</li></a>
+                        <ul class="mt-4">
+                            <a href="{{ route('user.profile', ['id' => Auth::id()]) }}"><li class="py-2 hover:bg-gray-700 hover:text-[color:#FF006E] px-4 rounded">Profile</li></a>
+                            <a href=""><li class="py-2 hover:bg-gray-700 hover:text-[color:#FF006E] px-4 rounded">Settings</li></a>
 
                             <hr class="border-gray-600 my-4">
 
