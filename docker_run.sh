f6e86f95f0a897f62ede0d50dce4ff7fe221defd
Rodrigo de Sousa <rodrigodesousa@proton.me>
2024-12-22 18:13:30 +0000

    docker run

--- docker_run.sh
+++ docker_run.sh
@@ -8,8 +8,8 @@ php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
+php artisan db:seed
 php artisan schedule:run
 php artisan storage:link
-php artisan db:seed
 php-fpm8.3 -D
 nginx -g "daemon off;"
